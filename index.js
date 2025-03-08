5c62c19d0763b92e95c47773911a2af5f51b82cb
Yampoy <yampoy.elvis@gmail.com>
2025-03-08 21:29:44 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Fix for multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // This should equal 62
 
-// Write your code here
+// Fix for random
+let random = Math.floor(Math.random() * 10) + 1; // Generates a random integer greater than 0
+
+// Fix for mod
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4; // This should equal 4
+
+// Fix for max
+let max = Math.max(1, 5, 10, 20); // This will return 20 as the highest number
